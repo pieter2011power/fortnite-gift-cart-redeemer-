0866288d40d10eb05cb5b6c39d3cf972c23c74ce
pieter2011power <61342871+pieter2011power@users.noreply.github.com>
2022-06-11 10:48:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input ('druk op enter om te beginnen') 
+for i in range(10456):
+  print('laden :', i)
+name = input('hai dit is een free fortnite gift card redeemer         druk op enter om te beginnen ongeveer bij dertig duizend vind ie vaak wat  \n')
+
+for i in range(43365):
+  print('zoeken  :', i)
+name = input('de code: RB99M-L34ED-lpLPI-GnVFT-IloLM code waarde 100 euro          \n')
